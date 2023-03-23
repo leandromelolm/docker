899234869aa2d674622895a2b02f20febaf81043
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-03-23 19:42:27 -0300

    Added flyway

---
+++ spring-postgres-flyway/backend/src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_person (user_id, username, email, password, created_on, last_login, birthdate) VALUES
+(10003,'username1', 'email1@email.com','123password', NOW(), NOW(), '2000-01-19'),
+(10005,'username2', 'email2@email.com','123password', NOW(), NOW(), '2000-01-20');
+
+
+-- NOW() - INTERVAL '25 MONTH'
+-- now() - INTERVAL '2 YEAR'
