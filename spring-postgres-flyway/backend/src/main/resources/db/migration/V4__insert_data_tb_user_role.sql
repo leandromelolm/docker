4903ddac30223a68af176f54d01e3ddb88901f73
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-03-24 11:51:27 -0300

    Migration added insert data_tb_role and data_tb_user_role

---
+++ spring-postgres-flyway/backend/src/main/resources/db/migration/V4__insert_data_tb_user_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user_role (user_id, role_id) VALUES (10003, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (10005, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (10005, 2);
